ef62201a0b42daaab421989f6f946222451cfcb4
Lifazz <148877192+Lifazz@users.noreply.github.com>
2024-02-18 18:02:39 +0700

    first commit

---
+++ Tugas/HitungUmur.py
@@ -0,0 +1,6 @@
+TahunLahir = int(input("Masukkan tahun lahir Anda : "))
+TahunSaatIni = int(input("Masukkan tahun saat ini : "))
+
+umur = TahunSaatIni - TahunLahir
+
+print("Umur Anda saat ini :", umur, "tahun")
